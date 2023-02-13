d559cf0bf0343abb62343d6a61364765271afcbd
BerenyiPeti <berenyipeti01@gmail.com>
2023-02-13 09:42:53 +0100

    alap metódusok

--- src/jarmu/Jarmu.java
+++ src/jarmu/Jarmu.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package jarmu;
 
-/**
- *
- * @author Péter
- */
 public class Jarmu {
+    private boolean beinditva, uzemanyag, megerkezett;
+
+    public void beindit() {
+        
+    }
     
+    public void leallit() {
+        
+    }
+    
+    public void tankol() {
+        
+    }
+    
+    public void halad() {
+        
+    }
 }
